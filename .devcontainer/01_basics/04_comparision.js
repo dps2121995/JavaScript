c309939871e5f2cf442a954550ab83409b3c6f3e
Dhirendra Pratap SINGH <65114453+dps2121995@users.noreply.github.com>
2025-07-09 21:16:17 +0000

    comparision

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,15 @@
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0) //False
+// console.log(null >= 0) //True
+// console.log(null == 0) //False
+// console.log(null === 0) //False
+
+
+// console.log("2" === 2) // False
+
+
+console.log(undefined > 0) //False
+console.log(undefined == 0) //False
+console.log(undefined >= 0) //False
