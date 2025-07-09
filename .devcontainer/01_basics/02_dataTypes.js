35c1f4f174282ed973d4e11a913a327ee96914e8
Dhirendra Pratap SINGH <65114453+dps2121995@users.noreply.github.com>
2025-07-09 20:23:46 +0000

    dataType

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+let name = "Singh" //String
+let age = 18 //Number
+//bigInt => 2 to power 53
+//boolean => True/False
+//null => Standalone value
+//undefined => not define yet
+//symbol => unique
+
+//object
+
+console.log(typeof "hit123");
+console.log(typeof null);
+console.log(typeof undefined)
+
+
