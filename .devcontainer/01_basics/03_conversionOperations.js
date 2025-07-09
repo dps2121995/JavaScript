8ec082df8433fe70346f839c6a13fa431fd1a4f8
Dhirendra Pratap SINGH <65114453+dps2121995@users.noreply.github.com>
2025-07-09 21:00:43 +0000

    converstionOperations

---
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -0,0 +1,47 @@
+let score=33
+console.log(typeof score)
+console.log(typeof (score))
+
+
+let scores=null
+console.log(typeof scores)
+let valueInNumber=Number(scores)
+console.log(typeof valueInNumber)
+
+
+let a=undefined
+let undefinedInNumber = Number(undefined)
+console.log(typeof undefinedInNumber)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof (stringNumber))
+
+
+
+let value=3
+let negvalue= -value
+console.log(negvalue)
+console.log(typeof negvalue)
+
+
+let str1="singh"
+let str="DP"
+str3=str+str1
+console.log(str3)
+
+console.log("1"+2) //12
+console.log(1+"2") //12
+
+console.log(1+2) //3
+console.log("1"+2+2) //122
+
+console.log(1+2+"2") //32
+
+console.log(true)
+console.log(+true)
+
+console.log(+"")
+
+console.log(...'Singh')
