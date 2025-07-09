48c6fabca3eb9cb8ce522ad96c1d6e35c1c553f6
Dhirendra Pratap SINGH <65114453+dps2121995@users.noreply.github.com>
2025-07-09 20:04:26 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId =12345
+let accountEmail="dps@google.com"
+var accountPassword="000"
+accountCity="Jaipur"
+/*
+accountId=2, Prefer not to use "var" because of issue in block scope and functional scope
+*/
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
